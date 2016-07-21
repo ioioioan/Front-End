27af5659c26615885ca93eb29dbeb4d0ed99f969
ioioioan <ioanj86@gmail.com>
2016-07-21 17:42:35 +0100

    1st commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+//shrink on scroll
+function init() {
+    window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 300,
+            header = document.querySelector("header");
+        if (distanceY > shrinkOn) {
+            classie.add(header,"smaller");
+        } else {
+            if (classie.has(header,"smaller")) {
+                classie.remove(header,"smaller");
+            }
+        }
+    });
+}
+window.onload = init();
+
+//accessibility controls for pausing video
+var vid = document.getElementById("bgvid"),
+pauseButton = document.getElementById("vidpause");
+function vidFade() {
+    vid.classList.add("stopfade");
+}
+vid.addEventListener('ended', function() {
+    // only functional if "loop" is removed 
+     vid.pause();
+	// to capture IE10
+	vidFade();
+});
+pauseButton.addEventListener("click", function() {
+    vid.classList.toggle("stopfade");
+	if (vid.paused) {
+vid.play();
+		pauseButton.innerHTML = "<span class=\"glyphicon glyphicon-pause\" aria-label=\"Pause\">";
+	} else {
+        vid.pause();
+        pauseButton.innerHTML = "<span class=\"glyphicon glyphicon-play\" aria-label=\"Play\">";
+	}
+})
